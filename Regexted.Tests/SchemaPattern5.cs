2a37aba0deee308659f08cc28521c05ee1c990cb
Alexander Stein <alexander.stein@nist.gov>
2022-02-11 11:00:26 -0500

    Update with reorganized library of unit tests.

---
+++ Regexted.Tests/SchemaPattern5.cs
@@ -0,0 +1,46 @@
+using Xunit;
+using System;
+using System.IO;
+using System.Xml.Linq;
+using Regexted;
+
+namespace Regexted.Tests
+{
+    public class SchemaPattern5 : IClassFixture<TestDataFixture>
+    {
+        TestDataFixture fixture;
+        const string pattern = "SchemaPattern5";
+
+        public SchemaPattern5(TestDataFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void Valid()
+        {
+            string schema = $"{fixture.contextPath}{pattern}.xsd";
+            XDocument document = XDocument.Load($"{fixture.contextPath}{pattern}Valid.xml");
+            XmlValidator validator = new XmlValidator(fixture.namespaceUri, fixture.namespacePrefix, schema, document);
+            validator.Validate();
+            Assert.True(
+                !validator.hasRuntimeErrors &&
+                !validator.hasValidationErrors &&
+                validator.hasCompletedValidation);
+        }
+
+        [Fact]
+        public void Invalid()
+        {
+            string schema = $"{fixture.contextPath}{pattern}.xsd";
+            XDocument document = XDocument.Load($"{fixture.contextPath}{pattern}Invalid.xml");
+            XmlValidator validator = new XmlValidator(fixture.namespaceUri, fixture.namespacePrefix, schema, document);
+            validator.Validate();
+            Assert.True(
+                !validator.hasRuntimeErrors &&
+                validator.hasCompletedValidation &&
+                validator.hasValidationErrors
+                );
+        }
+    }
+}
---
+++ Regexted.Tests/TestDataFixture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Regexted.Tests
+{
+    public class TestDataFixture : IDisposable
+    {
+        public DirectoryInfo contextPath { get; private set; }
+        public XNamespace namespaceUri { get; private set; }
+        public string namespacePrefix { get; private set; }
+
+        public TestDataFixture()
+        {
+            string sep = Path.DirectorySeparatorChar.ToString();
+            this.contextPath = new DirectoryInfo(
+                $"{System.AppDomain.CurrentDomain.BaseDirectory.ToString()}{sep}..{sep}..{sep}..{sep}..{sep}..{sep}Fixtures{sep}"
+            );
+            this.namespaceUri = "https://example.com/schema/1.0";
+            this.namespacePrefix = "example";
+        }
+
+        public void Dispose()
+        {
+            // Context data with configuration vars, nothing to destroy yet.
+            // This is need for the IDisposable interface though.
+        }
+    }
+}
